777d6246b5f830f67c9c5cbdd672bc8a06e3899e
spectrenoir06 <antoine@doussaud.org>
2015-07-15 17:12:27 +0200

    skybox.render

--- src/World.cpp
+++ src/World.cpp
@@ -27,7 +27,7 @@ void	World::add(Chunk const &chunk) {
 
 void	World::renderNear(glm::vec3 pos, Shader shader) {
 	for (size_t i = 0; i < _chunks.size(); i++) {
-		glm::vec3 temp = glm::vec3(pos.x / CHUNK_SIZE + 0.5f, pos.y / CHUNK_SIZE + 0.5f, pos.z / CHUNK_SIZE + 0.5f) - _chunkPos[i];
+		glm::vec3 temp = glm::vec3(pos.x / CHUNK_SIZE + 0.5f, pos.y / CHUNK_SIZE + 0.5f, pos.z / CHUNK_SIZE + 0.5f) - _chunkPos[i-1];
 		GLfloat distSqr = glm::dot(temp, temp);
 		if (sqrt(distSqr) < 2)
 			_chunks[i].render(shader);
