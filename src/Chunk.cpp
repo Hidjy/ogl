ebc22bdefadc6d3663ee135bf4d33391037f6ee2
Lou Hidjy <lou.hidjy@gmail.com>
2015-10-08 23:01:49 +0200

    AO v2

--- src/Chunk.cpp
+++ src/Chunk.cpp
@@ -158,107 +158,107 @@ void	Chunk::generateMesh() {
 					vz = static_cast<float>(z);
 					getRGBA(current.getType() - 1, r, g, b, a);
 
+					GLfloat ao[8] = {1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f};
+
+					if (y > 0 and z > 0 and _blocks[x][y - 1][z - 1].isActive()) {
+						ao[0] *= AO_COEFF;
+						ao[4] *= AO_COEFF;
+					}
+					if (y > 0 and x < Chunk::SIZE - 1 and _blocks[x + 1][y - 1][z].isActive()) {
+						ao[4] *= AO_COEFF;
+						ao[5] *= AO_COEFF;
+					}
+					if (y > 0 and z < Chunk::SIZE - 1 and _blocks[x][y - 1][z + 1].isActive()) {
+						ao[1] *= AO_COEFF;
+						ao[5] *= AO_COEFF;
+					}
+					if (y > 0 and x > 0 and _blocks[x - 1][y - 1][z].isActive()) {
+						ao[0] *= AO_COEFF;
+						ao[1] *= AO_COEFF;
+					}
+
+					if (y < Chunk::SIZE - 1 and z > 0 and _blocks[x][y + 1][z - 1].isActive()) {
+						ao[2] *= AO_COEFF;
+						ao[6] *= AO_COEFF;
+					}
+					if (y < Chunk::SIZE - 1 and x < Chunk::SIZE - 1 and _blocks[x + 1][y + 1][z].isActive()) {
+						ao[6] *= AO_COEFF;
+						ao[7] *= AO_COEFF;
+					}
+					if (y < Chunk::SIZE - 1 and z < Chunk::SIZE - 1 and _blocks[x][y + 1][z + 1].isActive()) {
+						ao[3] *= AO_COEFF;
+						ao[7] *= AO_COEFF;
+					}
+					if (y < Chunk::SIZE - 1 and x > 0 and _blocks[x - 1][y + 1][z].isActive()) {
+						ao[2] *= AO_COEFF;
+						ao[3] *= AO_COEFF;
+					}
+
+					if (x > 0 and y > 0 and z > 0 and _blocks[x - 1][y - 1][z - 1].isActive())
+						ao[0] *= AO_COEFF;
+					if (x > 0 and y > 0 and z < Chunk::SIZE - 1 and _blocks[x - 1][y - 1][z + 1].isActive())
+						ao[1] *= AO_COEFF;
+					if (x > 0 and y < Chunk::SIZE - 1 and z > 0 and _blocks[x - 1][y + 1][z - 1].isActive())
+						ao[2] *= AO_COEFF;
+					if (x > 0 and y < Chunk::SIZE - 1 and z < Chunk::SIZE - 1 and _blocks[x - 1][y + 1][z + 1].isActive())
+						ao[3] *= AO_COEFF;
+					if (x < Chunk::SIZE - 1 and y > 0 and z > 0 and _blocks[x + 1][y - 1][z - 1].isActive())
+						ao[4] *= AO_COEFF;
+					if (x < Chunk::SIZE - 1 and y > 0 and z < Chunk::SIZE - 1 and _blocks[x + 1][y - 1][z + 1].isActive())
+						ao[5] *= AO_COEFF;
+					if (x < Chunk::SIZE - 1 and y < Chunk::SIZE - 1 and z > 0 and _blocks[x + 1][y + 1][z - 1].isActive())
+						ao[6] *= AO_COEFF;
+					if (x < Chunk::SIZE - 1 and y < Chunk::SIZE - 1 and z < Chunk::SIZE - 1 and _blocks[x + 1][y + 1][z + 1].isActive())
+						ao[7] *= AO_COEFF;
+
 					if (x == 0 or not _blocks[x - 1][y][z].isActive()) {
-						GLfloat ao[4] = {1.0f, 1.0f, 1.0f, 1.0f};
-						if (x > 0 and z > 0 and _blocks[x - 1][y][z - 1].isActive())
-							ao[0] = AO_COEFF;
-						if (x > 0 and y > 0 and _blocks[x - 1][y - 1][z].isActive())
-							ao[1] = AO_COEFF;
-						if (x > 0 and z < Chunk::SIZE - 1 and _blocks[x - 1][y][z + 1].isActive())
-							ao[2] = AO_COEFF;
-						if (x > 0 and y < Chunk::SIZE - 1 and _blocks[x - 1][y + 1][z].isActive())
-							ao[3] = AO_COEFF;
-						multipush(vertices, {vx + 0.0f, vy + 0.0f, vz + 0.0f, r * ao[0] * ao[1], g * ao[0] * ao[1], b * ao[0] * ao[1], a});
-						multipush(vertices, {vx + 0.0f, vy + 0.0f, vz + 1.0f, r * ao[1] * ao[2], g * ao[1] * ao[2], b * ao[1] * ao[2], a});
-						multipush(vertices, {vx + 0.0f, vy + 1.0f, vz + 0.0f, r * ao[0] * ao[3], g * ao[0] * ao[3], b * ao[0] * ao[3], a});
-						multipush(vertices, {vx + 0.0f, vy + 1.0f, vz + 0.0f, r * ao[0] * ao[3], g * ao[0] * ao[3], b * ao[0] * ao[3], a});
-						multipush(vertices, {vx + 0.0f, vy + 0.0f, vz + 1.0f, r * ao[1] * ao[2], g * ao[1] * ao[2], b * ao[1] * ao[2], a});
-						multipush(vertices, {vx + 0.0f, vy + 1.0f, vz + 1.0f, r * ao[2] * ao[3], g * ao[2] * ao[3], b * ao[2] * ao[3], a});
+						multipush(vertices, {vx + 0.0f, vy + 0.0f, vz + 0.0f, r * ao[0], g * ao[0], b * ao[0], a});
+						multipush(vertices, {vx + 0.0f, vy + 0.0f, vz + 1.0f, r * ao[1], g * ao[1], b * ao[1], a});
+						multipush(vertices, {vx + 0.0f, vy + 1.0f, vz + 0.0f, r * ao[2], g * ao[2], b * ao[2], a});
+						multipush(vertices, {vx + 0.0f, vy + 1.0f, vz + 0.0f, r * ao[2], g * ao[2], b * ao[2], a});
+						multipush(vertices, {vx + 0.0f, vy + 0.0f, vz + 1.0f, r * ao[1], g * ao[1], b * ao[1], a});
+						multipush(vertices, {vx + 0.0f, vy + 1.0f, vz + 1.0f, r * ao[3], g * ao[3], b * ao[3], a});
 					}
 					if (y == 0 or not _blocks[x][y - 1][z].isActive()) {
-						GLfloat ao[4] = {1.0f, 1.0f, 1.0f, 1.0f};
-						if (y > 0 and z > 0 and _blocks[x][y - 1][z - 1].isActive())
-							ao[0] = AO_COEFF;
-						if (y > 0 and x < Chunk::SIZE - 1 and _blocks[x + 1][y - 1][z].isActive())
-							ao[1] = AO_COEFF;
-						if (y > 0 and z < Chunk::SIZE - 1 and _blocks[x][y - 1][z + 1].isActive())
-							ao[2] = AO_COEFF;
-						if (y > 0 and x > 0 and _blocks[x - 1][y - 1][z].isActive())
-							ao[3] = AO_COEFF;
-						multipush(vertices, {vx + 0.0f, vy + 0.0f, vz + 0.0f, r * ao[0] * ao[3], g * ao[0] * ao[3], b * ao[0] * ao[3], a});
-						multipush(vertices, {vx + 1.0f, vy + 0.0f, vz + 1.0f, r * ao[1] * ao[2], g * ao[1] * ao[2], b * ao[1] * ao[2], a});
-						multipush(vertices, {vx + 0.0f, vy + 0.0f, vz + 1.0f, r * ao[3] * ao[2], g * ao[3] * ao[2], b * ao[3] * ao[2], a});
-						multipush(vertices, {vx + 0.0f, vy + 0.0f, vz + 0.0f, r * ao[0] * ao[3], g * ao[0] * ao[3], b * ao[0] * ao[3], a});
-						multipush(vertices, {vx + 1.0f, vy + 0.0f, vz + 0.0f, r * ao[0] * ao[1], g * ao[0] * ao[1], b * ao[0] * ao[1], a});
-						multipush(vertices, {vx + 1.0f, vy + 0.0f, vz + 1.0f, r * ao[1] * ao[2], g * ao[1] * ao[2], b * ao[1] * ao[2], a});
+
+						multipush(vertices, {vx + 0.0f, vy + 0.0f, vz + 0.0f, r * ao[0], g * ao[0], b * ao[0], a});
+						multipush(vertices, {vx + 1.0f, vy + 0.0f, vz + 1.0f, r * ao[5], g * ao[5], b * ao[5], a});
+						multipush(vertices, {vx + 0.0f, vy + 0.0f, vz + 1.0f, r * ao[1], g * ao[1], b * ao[1], a});
+						multipush(vertices, {vx + 0.0f, vy + 0.0f, vz + 0.0f, r * ao[0], g * ao[0], b * ao[0], a});
+						multipush(vertices, {vx + 1.0f, vy + 0.0f, vz + 0.0f, r * ao[4], g * ao[4], b * ao[4], a});
+						multipush(vertices, {vx + 1.0f, vy + 0.0f, vz + 1.0f, r * ao[5], g * ao[5], b * ao[5], a});
 					}
 					if (z == 0 or not _blocks[x][y][z - 1].isActive()) {
-						GLfloat ao[4] = {1.0f, 1.0f, 1.0f, 1.0f};
-						if (z > 0 and y > 0 and _blocks[x][y - 1][z - 1].isActive())
-							ao[0] = AO_COEFF;
-						if (z > 0 and x > 0 and _blocks[x - 1][y][z - 1].isActive())
-							ao[1] = AO_COEFF;
-						if (z > 0 and y < Chunk::SIZE - 1 and _blocks[x][y + 1][z - 1].isActive())
-							ao[2] = AO_COEFF;
-						if (z > 0 and x < Chunk::SIZE - 1 and _blocks[x + 1][y][z - 1].isActive())
-							ao[3] = AO_COEFF;
-						multipush(vertices, {vx + 0.0f, vy + 0.0f, vz + 0.0f, r * ao[0] * ao[1], g * ao[0] * ao[1], b * ao[0] * ao[1], a});
-						multipush(vertices, {vx + 0.0f, vy + 1.0f, vz + 0.0f, r * ao[1] * ao[2], g * ao[1] * ao[2], b * ao[1] * ao[2], a});
-						multipush(vertices, {vx + 1.0f, vy + 0.0f, vz + 0.0f, r * ao[0] * ao[3], g * ao[0] * ao[3], b * ao[0] * ao[3], a});
-						multipush(vertices, {vx + 1.0f, vy + 0.0f, vz + 0.0f, r * ao[0] * ao[3], g * ao[0] * ao[3], b * ao[0] * ao[3], a});
-						multipush(vertices, {vx + 0.0f, vy + 1.0f, vz + 0.0f, r * ao[1] * ao[2], g * ao[1] * ao[2], b * ao[1] * ao[2], a});
-						multipush(vertices, {vx + 1.0f, vy + 1.0f, vz + 0.0f, r * ao[2] * ao[3], g * ao[2] * ao[3], b * ao[2] * ao[3], a});
+						multipush(vertices, {vx + 0.0f, vy + 0.0f, vz + 0.0f, r * ao[0], g * ao[0], b * ao[0], a});
+						multipush(vertices, {vx + 0.0f, vy + 1.0f, vz + 0.0f, r * ao[2], g * ao[2], b * ao[2], a});
+						multipush(vertices, {vx + 1.0f, vy + 0.0f, vz + 0.0f, r * ao[4], g * ao[4], b * ao[4], a});
+						multipush(vertices, {vx + 1.0f, vy + 0.0f, vz + 0.0f, r * ao[4], g * ao[4], b * ao[4], a});
+						multipush(vertices, {vx + 0.0f, vy + 1.0f, vz + 0.0f, r * ao[2], g * ao[2], b * ao[2], a});
+						multipush(vertices, {vx + 1.0f, vy + 1.0f, vz + 0.0f, r * ao[6], g * ao[6], b * ao[6], a});
 					}
 					if (x == Chunk::SIZE - 1 or not _blocks[x + 1][y][z].isActive()) {
-						GLfloat ao[4] = {1.0f, 1.0f, 1.0f, 1.0f};
-						if (x < Chunk::SIZE - 1 and z > 0 and _blocks[x + 1][y][z - 1].isActive())
-							ao[0] = AO_COEFF;
-						if (x < Chunk::SIZE - 1 and y > 0 and _blocks[x + 1][y - 1][z].isActive())
-							ao[1] = AO_COEFF;
-						if (x < Chunk::SIZE - 1 and z < Chunk::SIZE - 1 and _blocks[x + 1][y][z + 1].isActive())
-							ao[2] = AO_COEFF;
-						if (x < Chunk::SIZE - 1 and y < Chunk::SIZE - 1 and _blocks[x + 1][y + 1][z].isActive())
-							ao[3] = AO_COEFF;
-						multipush(vertices, {vx + 1.0f, vy + 1.0f, vz + 1.0f, r * ao[2] * ao[3], g * ao[2] * ao[3], b * ao[2] * ao[3], a});
-						multipush(vertices, {vx + 1.0f, vy + 0.0f, vz + 1.0f, r * ao[1] * ao[2], g * ao[1] * ao[2], b * ao[1] * ao[2], a});
-						multipush(vertices, {vx + 1.0f, vy + 1.0f, vz + 0.0f, r * ao[0] * ao[3], g * ao[0] * ao[3], b * ao[0] * ao[3], a});
-						multipush(vertices, {vx + 1.0f, vy + 0.0f, vz + 0.0f, r * ao[0] * ao[1], g * ao[0] * ao[1], b * ao[0] * ao[1], a});
-						multipush(vertices, {vx + 1.0f, vy + 1.0f, vz + 0.0f, r * ao[0] * ao[3], g * ao[0] * ao[3], b * ao[0] * ao[3], a});
-						multipush(vertices, {vx + 1.0f, vy + 0.0f, vz + 1.0f, r * ao[1] * ao[2], g * ao[1] * ao[2], b * ao[1] * ao[2], a});
+						multipush(vertices, {vx + 1.0f, vy + 1.0f, vz + 1.0f, r * ao[7], g * ao[7], b * ao[7], a});
+						multipush(vertices, {vx + 1.0f, vy + 0.0f, vz + 1.0f, r * ao[5], g * ao[5], b * ao[5], a});
+						multipush(vertices, {vx + 1.0f, vy + 1.0f, vz + 0.0f, r * ao[6], g * ao[6], b * ao[6], a});
+						multipush(vertices, {vx + 1.0f, vy + 0.0f, vz + 0.0f, r * ao[4], g * ao[4], b * ao[4], a});
+						multipush(vertices, {vx + 1.0f, vy + 1.0f, vz + 0.0f, r * ao[6], g * ao[6], b * ao[6], a});
+						multipush(vertices, {vx + 1.0f, vy + 0.0f, vz + 1.0f, r * ao[5], g * ao[5], b * ao[5], a});
 					}
 					if (y == Chunk::SIZE - 1 or not _blocks[x][y + 1][z].isActive()) {
-						GLfloat ao[4] = {1.0f, 1.0f, 1.0f, 1.0f};
-						if (y < Chunk::SIZE - 1 and z > 0 and _blocks[x][y + 1][z - 1].isActive())
-							ao[0] = AO_COEFF;
-						if (y < Chunk::SIZE - 1 and x < Chunk::SIZE - 1 and _blocks[x + 1][y + 1][z].isActive())
-							ao[1] = AO_COEFF;
-						if (y < Chunk::SIZE - 1 and z < Chunk::SIZE - 1 and _blocks[x][y + 1][z + 1].isActive())
-							ao[2] = AO_COEFF;
-						if (y < Chunk::SIZE - 1 and x > 0 and _blocks[x - 1][y + 1][z].isActive())
-							ao[3] = AO_COEFF;
-						multipush(vertices, {vx + 1.0f, vy + 1.0f, vz + 1.0f, r * ao[1] * ao[2], g * ao[1] * ao[2], b * ao[1] * ao[2], a});
-						multipush(vertices, {vx + 0.0f, vy + 1.0f, vz + 0.0f, r * ao[0] * ao[3], g * ao[0] * ao[3], b * ao[0] * ao[3], a});
-						multipush(vertices, {vx + 0.0f, vy + 1.0f, vz + 1.0f, r * ao[2] * ao[3], g * ao[2] * ao[3], b * ao[2] * ao[3], a});
-						multipush(vertices, {vx + 1.0f, vy + 1.0f, vz + 1.0f, r * ao[1] * ao[2], g * ao[1] * ao[2], b * ao[1] * ao[2], a});
-						multipush(vertices, {vx + 1.0f, vy + 1.0f, vz + 0.0f, r * ao[0] * ao[1], g * ao[0] * ao[1], b * ao[0] * ao[1], a});
-						multipush(vertices, {vx + 0.0f, vy + 1.0f, vz + 0.0f, r * ao[0] * ao[3], g * ao[0] * ao[3], b * ao[0] * ao[3], a});
+						multipush(vertices, {vx + 1.0f, vy + 1.0f, vz + 1.0f, r * ao[7], g * ao[7], b * ao[7], a});
+						multipush(vertices, {vx + 0.0f, vy + 1.0f, vz + 0.0f, r * ao[2], g * ao[2], b * ao[2], a});
+						multipush(vertices, {vx + 0.0f, vy + 1.0f, vz + 1.0f, r * ao[3], g * ao[3], b * ao[3], a});
+						multipush(vertices, {vx + 1.0f, vy + 1.0f, vz + 1.0f, r * ao[7], g * ao[7], b * ao[7], a});
+						multipush(vertices, {vx + 1.0f, vy + 1.0f, vz + 0.0f, r * ao[6], g * ao[6], b * ao[6], a});
+						multipush(vertices, {vx + 0.0f, vy + 1.0f, vz + 0.0f, r * ao[2], g * ao[2], b * ao[2], a});
 					}
 					if (z == Chunk::SIZE - 1 or not _blocks[x][y][z + 1].isActive()) {
-						GLfloat ao[4] = {1.0f, 1.0f, 1.0f, 1.0f};
-						if (z < Chunk::SIZE - 1 and y > 0 and _blocks[x][y - 1][z + 1].isActive())
-							ao[0] = AO_COEFF;
-						if (z < Chunk::SIZE - 1 and x > 0 and _blocks[x - 1][y][z + 1].isActive())
-							ao[1] = AO_COEFF;
-						if (z < Chunk::SIZE - 1 and y < Chunk::SIZE - 1 and _blocks[x][y + 1][z + 1].isActive())
-							ao[2] = AO_COEFF;
-						if (z < Chunk::SIZE - 1 and x < Chunk::SIZE - 1 and _blocks[x + 1][y][z + 1].isActive())
-							ao[3] = AO_COEFF;
-						multipush(vertices, {vx + 1.0f, vy + 1.0f, vz + 1.0f, r * ao[0] * ao[3], g * ao[0] * ao[3], b * ao[0] * ao[3], a});
-						multipush(vertices, {vx + 0.0f, vy + 1.0f, vz + 1.0f, r * ao[1] * ao[2], g * ao[1] * ao[2], b * ao[1] * ao[2], a});
-						multipush(vertices, {vx + 1.0f, vy + 0.0f, vz + 1.0f, r * ao[0] * ao[3], g * ao[0] * ao[3], b * ao[0] * ao[3], a});
-						multipush(vertices, {vx + 0.0f, vy + 0.0f, vz + 1.0f, r * ao[0] * ao[1], g * ao[0] * ao[1], b * ao[0] * ao[1], a});
-						multipush(vertices, {vx + 1.0f, vy + 0.0f, vz + 1.0f, r * ao[0] * ao[3], g * ao[0] * ao[3], b * ao[0] * ao[3], a});
-						multipush(vertices, {vx + 0.0f, vy + 1.0f, vz + 1.0f, r * ao[1] * ao[2], g * ao[1] * ao[2], b * ao[1] * ao[2], a});
+						multipush(vertices, {vx + 1.0f, vy + 1.0f, vz + 1.0f, r * ao[7], g * ao[7], b * ao[7], a});
+						multipush(vertices, {vx + 0.0f, vy + 1.0f, vz + 1.0f, r * ao[3], g * ao[3], b * ao[3], a});
+						multipush(vertices, {vx + 1.0f, vy + 0.0f, vz + 1.0f, r * ao[5], g * ao[5], b * ao[5], a});
+						multipush(vertices, {vx + 0.0f, vy + 0.0f, vz + 1.0f, r * ao[1], g * ao[1], b * ao[1], a});
+						multipush(vertices, {vx + 1.0f, vy + 0.0f, vz + 1.0f, r * ao[5], g * ao[5], b * ao[5], a});
+						multipush(vertices, {vx + 0.0f, vy + 1.0f, vz + 1.0f, r * ao[3], g * ao[3], b * ao[3], a});
 					}
 				}
 			}
